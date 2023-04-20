f65e6238c1a360822b7b2c5ff3d9b70484a55991
RBF <tarans.fishborn@gmail.com>
2023-04-20 22:01:07 +0300

    some changes

--- blog/config.py
+++ blog/config.py
@@ -9,7 +9,7 @@ load_dotenv()
 ENV = os.getenv('flask_venv', default=EnvType.production)
 DEBUG = ENV == EnvType.development
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "abcdefg123456"
 
 SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/blog.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
