a15388876c955019e1ab2d86a0918c6850838230
RBF <tarans.fishborn@gmail.com>
2023-04-26 19:15:59 +0300

    create models for authors and articles

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from flask_login import UserMixin
 from blog.extensions import db
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
 
 
 class User(db.Model, UserMixin):
@@ -16,3 +19,25 @@ class User(db.Model, UserMixin):
         self.password = password
         self.first_name = first_name
         self.last_name = last_name
+
+class Author(db.Model):
+    __tablename__ = 'authors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('users.id'), nullable=False)
+
+    user = relationship('User', back_populates='author')
+    articles = relationship('Article', back_populates='author')
+
+
+class Article(db.Model):
+    __tablename__ = 'articles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, ForeignKey('authors.id'), nullable=False)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    author = relationship('Author', back_populates='articles')
